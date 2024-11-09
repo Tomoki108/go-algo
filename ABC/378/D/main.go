6d4e18675caf73458b893711d0e737fec34f07cc
Tomoki108 <shinonome81@gmail.com>
2024-11-09 15:47:52 +0900

    solve  D

---
+++ ABC/378/D/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var H, W, K int
+var grid [][]string
+var visited [][]bool
+var ans int
+
+func main() {
+	fmt.Scanf("%d %d %d", &H, &W, &K)
+
+	grid = make([][]string, H)
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < H; i++ {
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimSpace(line) // remove '\n' (TrimSpaceは改行も削除してくれる)
+		grid[i] = strings.Split(line, "")
+	}
+
+	visited = make([][]bool, H)
+	for h := 0; h < H; h++ {
+		visited[h] = make([]bool, W)
+		for w := 0; w < W; w++ {
+			visited[h][w] = false
+		}
+	}
+
+	for h := 0; h < H; h++ {
+		for w := 0; w < W; w++ {
+			if grid[h][w] == "." {
+				exploere(h, w, 0)
+			}
+		}
+	}
+
+	fmt.Println(ans)
+}
+
+func exploere(i, j, k int) {
+	if k == K {
+		ans++
+		return
+	}
+
+	visited[i][j] = true
+
+	for _, delta := range [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
+		ni, nj := i+delta[0], j+delta[1]
+
+		if 0 <= ni && ni < H && 0 <= nj && nj < W && grid[ni][nj] == "." && !visited[ni][nj] {
+			exploere(ni, nj, k+1)
+		}
+
+	}
+
+	// ここに到達するのは、ここからはそれ以上もうどこにも進めない場合。訪問済みを解除して、再帰元に戻る
+	visited[i][j] = false
+}
