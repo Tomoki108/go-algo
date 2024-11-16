4e2c6bbd2c6c0fed317192bd5df30d42a5065179
Tomoki108 <shinonome81@gmail.com>
2024-11-16 20:45:31 +0900

    遅延評価の導入、d solved

--- ABC/379/D/main.go
+++ ABC/379/D/main.go
@@ -14,35 +14,27 @@ func main() {
 	fmt.Scan(&Q)
 
 	plantHeights := make([]int, 0)
+	totalGrowth := 0 // 遅延評価用の成長値
 
 	reader := bufio.NewReader(os.Stdin)
 	for i := 0; i < Q; i++ {
-
 		line, _ := reader.ReadString('\n')
 		sl := strings.Fields(line)
 
 		if sl[0] == "1" {
-			plantHeights = append(plantHeights, 0)
+			plantHeights = append(plantHeights, -totalGrowth) // 遅延成長分を差し引いて追加
 		} else if sl[0] == "2" {
 			t, _ := strconv.Atoi(sl[1])
-
-			waited := make([]int, 0, len(plantHeights))
-			for _, height := range plantHeights {
-				waited = append(waited, height+t)
-			}
-			plantHeights = waited
+			totalGrowth += t
 		} else if sl[0] == "3" {
 			h, _ := strconv.Atoi(sl[1])
+			h -= totalGrowth // 実際の高さに変換
 
-			sort.Slice(plantHeights, func(i, j int) bool {
-				return plantHeights[i] < plantHeights[j]
-			})
-
+			sort.Ints(plantHeights)
 			idx := sort.Search(len(plantHeights), func(i int) bool {
 				return plantHeights[i] >= h
 			})
 			fmt.Println(len(plantHeights) - idx)
-
 			plantHeights = plantHeights[:idx]
 		}
 	}
