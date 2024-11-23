deb4878d82f442944b32464d5159f51b05cbc67a
Tomoki108 <shinonome81@gmail.com>
2024-11-23 19:33:33 +0900

    solve B

--- ABC/376/B/main.go
+++ ABC/376/B/main.go
@@ -2,17 +2,63 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-var reader = bufio.NewReader(os.Stdin)
-var writer = bufio.NewWriter(os.Stdout)
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
 
+// ポイント：円について、from~toが1~(1+距離)になるように回転して調節する。from:8 to:1のような状況は考えづらいため。
+// from < toではない時にfromとtoを入れ替えるやり方も可。位置が入れ替わっても、移動コストや移動可能な道は変わらない。図を書くと分かる。
 func main() {
-	defer writer.Flush()
+	defer w.Flush()
 
+	strs := ReadIntArr(r)
+	N := strs[0]
+	Q := strs[1]
+
+	L := 1
+	R := 2
+	cost := 0
+	for i := 0; i < Q; i++ {
+		strs := ReadStrArr(r)
+		H := strs[0]
+		T, _ := strconv.Atoi(strs[1])
+
+		if H == "L" {
+			cost += getCost(L, T, R, N)
+			L = T
+		} else {
+			cost += getCost(R, T, L, N)
+			R = T
+		}
+	}
+
+	fmt.Fprint(w, cost)
+}
+
+func getCost(from, to, ng, N int) int {
+	// from = 1 になるように円を回転させる
+	diff := from - 1
+	from = 1
+	to = to - diff
+	if to < 1 {
+		to = N + to
+	}
+
+	ng = ng - diff
+	if ng < 1 {
+		ng = N + ng
+	}
+
+	if to < ng {
+		return to - from
+	}
+
+	return N - (to - from)
 }
 
 //////////////
