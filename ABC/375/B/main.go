74787243ff77fd61ff4eedaf3388a4dd10a2006f
Tomoki108 <shinonome81@gmail.com>
2024-11-25 17:33:58 +0900

    solve B

--- ABC/375/B/main.go
+++ ABC/375/B/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +15,22 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N := ReadInt(r)
+
+	currentX := 0
+	currentY := 0
+	var cost float64
+	for i := 0; i < N; i++ {
+		X, Y := ReadTwoInts(r)
+
+		cost += math.Sqrt(float64((X-currentX)*(X-currentX) + (Y-currentY)*(Y-currentY)))
+		currentX = X
+		currentY = Y
+	}
+
+	cost += math.Sqrt(float64(currentX*currentX + currentY*currentY))
+
+	fmt.Fprint(w, cost)
 }
 
 //////////////
@@ -33,6 +51,16 @@ func ReadInt(r *bufio.Reader) int {
 	i, _ := strconv.Atoi(str)
 
 	return i
+}
+
+// 一行に2つの整数のみの入力を読み込む
+func ReadTwoInts(r *bufio.Reader) (int, int) {
+	input, _ := r.ReadString('\n')
+	strs := strings.Fields(input)
+	i1, _ := strconv.Atoi(strs[0])
+	i2, _ := strconv.Atoi(strs[1])
+
+	return i1, i2
 }
 
 // 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
--- ABC/main.go
+++ ABC/main.go
@@ -35,6 +35,16 @@ func ReadInt(r *bufio.Reader) int {
 	return i
 }
 
+// 一行に2つの整数のみの入力を読み込む
+func ReadTwoInts(r *bufio.Reader) (int, int) {
+	input, _ := r.ReadString('\n')
+	strs := strings.Fields(input)
+	i1, _ := strconv.Atoi(strs[0])
+	i2, _ := strconv.Atoi(strs[1])
+
+	return i1, i2
+}
+
 // 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
 func ReadStrArr(r *bufio.Reader) []string {
 	input, _ := r.ReadString('\n')
