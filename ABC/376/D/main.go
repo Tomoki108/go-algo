302c9f458ec9c2b3f6c03d28fc99c25a99d7b5be
Tomoki108 <shinonome81@gmail.com>
2024-11-25 16:40:31 +0900

    solve D

--- ABC/376/D/main.go
+++ ABC/376/D/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"container/list"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +15,64 @@ var writer = bufio.NewWriter(os.Stdout)
 func main() {
 	defer writer.Flush()
 
+	ints := ReadIntArr(reader)
+	N := ints[0]
+	M := ints[1]
+
+	var start = 1
+	edges := make(map[int][]int, M)
+	m := make(map[int]bool, N) // そのノードからstartへ行けるかどうか
+
+	for i := 0; i < M; i++ {
+		ints := ReadIntArr(reader)
+		from := ints[0]
+		to := ints[1]
+
+		edges[from] = append(edges[from], to)
+		if to == start {
+			m[from] = true
+		}
+	}
+
+	visited := make(map[int]bool) // 訪問済みノードを記録
+
+	queue := list.New() // BFSのためのキュー
+	type elm struct {   // queueに入れる要素
+		node  int
+		depth int
+	}
+
+	queue.PushBack(elm{node: start, depth: 0})
+	visited[start] = true
+
+	foundDepth := 0
+	// 頂点1からBFSし、頂点1に行けるノードを見つける
+Outer:
+	for queue.Len() > 0 {
+		current := queue.Front().Value.(elm)
+		queue.Remove(queue.Front())
+
+		dests := edges[current.node]
+		for _, dest := range dests {
+			if visited[dest] {
+				continue
+			}
+			visited[dest] = true
+
+			if m[dest] {
+				foundDepth = current.depth + 1
+				break Outer
+			}
+
+			queue.PushBack(elm{node: dest, depth: current.depth + 1})
+		}
+	}
+
+	if foundDepth > 0 {
+		fmt.Fprint(writer, foundDepth+1) // 頂点1に行けるノードの深さ+1が閉路の辺数
+	} else {
+		fmt.Fprint(writer, -1)
+	}
 }
 
 //////////////
