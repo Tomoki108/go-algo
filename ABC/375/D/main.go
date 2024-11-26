3887b37ded0f15472e6122e2f92b5642e906b0ed
Tomoki108 <shinonome81@gmail.com>
2024-11-26 15:37:09 +0900

    solve D

--- ABC/375/D/main.go
+++ ABC/375/D/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +15,35 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	S := readString(r)
+	ss := strings.Split(S, "")
+
+	ans := 0
+	m := make(map[string][]int) // 各アルファベットが登場する座標
+	m2 := make(map[string]int)  // 各アルファベットの、最後に計算したそれを頭と尻で使う回文の組み合わせの数
+	for i, s := range ss {
+		if len(m[s]) == 0 {
+			m[s] = append(m[s], i)
+			continue
+		}
+
+		if len(m[s]) == 1 {
+			pattern := i - m[s][0] - 1
+			ans += pattern
+			m[s] = append(m[s], i)
+			m2[s] = pattern
+			continue
+		}
+
+		lastSIndex := m[s][len(m[s])-1]
+		pattern := m2[s] + (i-lastSIndex)*(len(m[s])-1) + (i - lastSIndex - 1)
+
+		ans += pattern
+		m[s] = append(m[s], i)
+		m2[s] = pattern
+	}
+
+	fmt.Fprint(w, ans)
 }
 
 //////////////
@@ -20,14 +51,14 @@ func main() {
 /////////////
 
 // 一行に1文字のみの入力を読み込む
-func ReadString(r *bufio.Reader) string {
+func readString(r *bufio.Reader) string {
 	input, _ := r.ReadString('\n')
 
 	return strings.TrimSpace(input)
 }
 
 // 一行に1つの整数のみの入力を読み込む
-func ReadInt(r *bufio.Reader) int {
+func readInt(r *bufio.Reader) int {
 	input, _ := r.ReadString('\n')
 	str := strings.TrimSpace(input)
 	i, _ := strconv.Atoi(str)
@@ -35,20 +66,24 @@ func ReadInt(r *bufio.Reader) int {
 	return i
 }
 
-// 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
-func ReadStrArr(r *bufio.Reader) []string {
+// 一行に2つの整数のみの入力を読み込む
+func read2Ints(r *bufio.Reader) (int, int) {
 	input, _ := r.ReadString('\n')
 	strs := strings.Fields(input)
-	arr := make([]string, len(strs))
-	for i, s := range strs {
-		arr[i] = s
-	}
+	i1, _ := strconv.Atoi(strs[0])
+	i2, _ := strconv.Atoi(strs[1])
 
-	return arr
+	return i1, i2
+}
+
+// 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
+func readStrArr(r *bufio.Reader) []string {
+	input, _ := r.ReadString('\n')
+	return strings.Fields(input)
 }
 
 // 一行に複数の整数が入力される場合、スペース区切りで整数を読み込む
-func ReadIntArr(r *bufio.Reader) []int {
+func readIntArr(r *bufio.Reader) []int {
 	input, _ := r.ReadString('\n')
 	strs := strings.Fields(input)
 	arr := make([]int, len(strs))
@@ -57,4 +92,60 @@ func ReadIntArr(r *bufio.Reader) []int {
 	}
 
 	return arr
+}
+
+// height行の文字列グリッドを読み込む
+func readGrid(r *bufio.Reader, height int) [][]string {
+	grid := make([][]string, height)
+	for i := 0; i < height; i++ {
+		grid[i] = readStrArr(r)
+	}
+
+	return grid
+}
+
+// 文字列グリッドを出力する
+func writeGrid(w *bufio.Writer, grid [][]string) {
+	for i := 0; i < len(grid); i++ {
+		fmt.Fprint(w, strings.Join(grid[i], ""), "\n")
+	}
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)を、時計回りにtime回回転させたときの座標を返す
+func rotateGridCell(n, height, width, time int) (h, w int) {
+	time = time % 4
+	switch time {
+	case 0:
+		return height, width
+	case 1:
+		return width, n - height + 1
+	case 2:
+		return n - height + 1, n - width + 1
+	case 3:
+		return n - width + 1, height
+	}
+
+	panic("can't reach here")
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)が、最も外側のマス目達を1周目としたときに何周目にあるかを返す
+func getGridCellLayer(n, h, w int) int {
+	return int(math.Min(math.Min(float64(h), float64(w)), math.Min(float64(n-h+1), float64(n-w+1))))
+}
+
+// nCrの計算 O(r)
+// (n * (n-1) ... * (n-r+1)) / r!
+func combination(n, r int) int {
+	if r > n {
+		return 0
+	}
+	if r > n/2 {
+		r = n - r // Use smaller r for efficiency
+	}
+	result := 1
+	for i := 0; i < r; i++ {
+		result *= (n - i)
+		result /= (i + 1)
+	}
+	return result
 }
--- ABC/main.go
+++ ABC/main.go
@@ -103,3 +103,20 @@ func rotateGridCell(n, height, width, time int) (h, w int) {
 func getGridCellLayer(n, h, w int) int {
 	return int(math.Min(math.Min(float64(h), float64(w)), math.Min(float64(n-h+1), float64(n-w+1))))
 }
+
+// nCrの計算 O(r)
+// (n * (n-1) ... * (n-r+1)) / r!
+func combination(n, r int) int {
+	if r > n {
+		return 0
+	}
+	if r > n/2 {
+		r = n - r // Use smaller r for efficiency
+	}
+	result := 1
+	for i := 0; i < r; i++ {
+		result *= (n - i)
+		result /= (i + 1)
+	}
+	return result
+}
