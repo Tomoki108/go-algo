42bb3b59d8fc131b5073677fb504d14b08d93d3c
Tomoki108 <shinonome81@gmail.com>
2024-11-26 14:23:02 +0900

    solve C

--- ABC/375/C/main.go
+++ ABC/375/C/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +15,51 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N := ReadInt(r)
+
+	grid := make([][]string, N)
+	updatedGrid := make([][]string, N)
+	for i := 0; i < N; i++ {
+		s := ReadString(r)
+		grid[i] = strings.Split(s, "")
+		updatedGrid[i] = make([]string, N)
+	}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			h := i + 1
+			w := j + 1
+
+			rotateTime := getGridCellLayer(N, h, w)
+			destH, destW := rotateGridCell(N, h, w, int(rotateTime))
+			updatedGrid[destH-1][destW-1] = grid[i][j]
+		}
+
+	}
+
+	WriteGrid(w, updatedGrid)
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)を、時計回りにtime回回転させたときの座標を返す
+func rotateGridCell(n, height, width, time int) (h, w int) {
+	time = time % 4
+	switch time {
+	case 0:
+		return height, width
+	case 1:
+		return width, n - height + 1
+	case 2:
+		return n - height + 1, n - width + 1
+	case 3:
+		return n - width + 1, height
+	}
+
+	panic("can't reach here")
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)が、最も外側のマス目達を1周目としたときに何周目にあるかを返す
+func getGridCellLayer(n, h, w int) int {
+	return int(math.Min(math.Min(float64(h), float64(w)), math.Min(float64(n-h+1), float64(n-w+1))))
 }
 
 //////////////
@@ -35,16 +82,20 @@ func ReadInt(r *bufio.Reader) int {
 	return i
 }
 
+// 一行に2つの整数のみの入力を読み込む
+func ReadTwoInts(r *bufio.Reader) (int, int) {
+	input, _ := r.ReadString('\n')
+	strs := strings.Fields(input)
+	i1, _ := strconv.Atoi(strs[0])
+	i2, _ := strconv.Atoi(strs[1])
+
+	return i1, i2
+}
+
 // 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
 func ReadStrArr(r *bufio.Reader) []string {
 	input, _ := r.ReadString('\n')
-	strs := strings.Fields(input)
-	arr := make([]string, len(strs))
-	for i, s := range strs {
-		arr[i] = s
-	}
-
-	return arr
+	return strings.Fields(input)
 }
 
 // 一行に複数の整数が入力される場合、スペース区切りで整数を読み込む
@@ -57,4 +108,21 @@ func ReadIntArr(r *bufio.Reader) []int {
 	}
 
 	return arr
+}
+
+// height行の文字列グリッドを読み込む
+func ReadGrid(r *bufio.Reader, height int) [][]string {
+	grid := make([][]string, height)
+	for i := 0; i < height; i++ {
+		grid[i] = ReadStrArr(r)
+	}
+
+	return grid
+}
+
+// 文字列グリッドを出力する
+func WriteGrid(w *bufio.Writer, grid [][]string) {
+	for i := 0; i < len(grid); i++ {
+		fmt.Fprint(w, strings.Join(grid[i], ""), "\n")
+	}
 }
--- ABC/main.go
+++ ABC/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -20,14 +22,14 @@ func main() {
 /////////////
 
 // 一行に1文字のみの入力を読み込む
-func ReadString(r *bufio.Reader) string {
+func readString(r *bufio.Reader) string {
 	input, _ := r.ReadString('\n')
 
 	return strings.TrimSpace(input)
 }
 
 // 一行に1つの整数のみの入力を読み込む
-func ReadInt(r *bufio.Reader) int {
+func readInt(r *bufio.Reader) int {
 	input, _ := r.ReadString('\n')
 	str := strings.TrimSpace(input)
 	i, _ := strconv.Atoi(str)
@@ -36,7 +38,7 @@ func ReadInt(r *bufio.Reader) int {
 }
 
 // 一行に2つの整数のみの入力を読み込む
-func ReadTwoInts(r *bufio.Reader) (int, int) {
+func read2Ints(r *bufio.Reader) (int, int) {
 	input, _ := r.ReadString('\n')
 	strs := strings.Fields(input)
 	i1, _ := strconv.Atoi(strs[0])
@@ -46,19 +48,13 @@ func ReadTwoInts(r *bufio.Reader) (int, int) {
 }
 
 // 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
-func ReadStrArr(r *bufio.Reader) []string {
+func readStrArr(r *bufio.Reader) []string {
 	input, _ := r.ReadString('\n')
-	strs := strings.Fields(input)
-	arr := make([]string, len(strs))
-	for i, s := range strs {
-		arr[i] = s
-	}
-
-	return arr
+	return strings.Fields(input)
 }
 
 // 一行に複数の整数が入力される場合、スペース区切りで整数を読み込む
-func ReadIntArr(r *bufio.Reader) []int {
+func readIntArr(r *bufio.Reader) []int {
 	input, _ := r.ReadString('\n')
 	strs := strings.Fields(input)
 	arr := make([]int, len(strs))
@@ -67,4 +63,43 @@ func ReadIntArr(r *bufio.Reader) []int {
 	}
 
 	return arr
+}
+
+// height行の文字列グリッドを読み込む
+func readGrid(r *bufio.Reader, height int) [][]string {
+	grid := make([][]string, height)
+	for i := 0; i < height; i++ {
+		grid[i] = readStrArr(r)
+	}
+
+	return grid
+}
+
+// 文字列グリッドを出力する
+func writeGrid(w *bufio.Writer, grid [][]string) {
+	for i := 0; i < len(grid); i++ {
+		fmt.Fprint(w, strings.Join(grid[i], ""), "\n")
+	}
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)を、時計回りにtime回回転させたときの座標を返す
+func rotateGridCell(n, height, width, time int) (h, w int) {
+	time = time % 4
+	switch time {
+	case 0:
+		return height, width
+	case 1:
+		return width, n - height + 1
+	case 2:
+		return n - height + 1, n - width + 1
+	case 3:
+		return n - width + 1, height
+	}
+
+	panic("can't reach here")
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)が、最も外側のマス目達を1周目としたときに何周目にあるかを返す
+func getGridCellLayer(n, h, w int) int {
+	return int(math.Min(math.Min(float64(h), float64(w)), math.Min(float64(n-h+1), float64(n-w+1))))
 }
