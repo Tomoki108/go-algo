dce5a12cfb60312064737130ed7d4e5b5f3f8cdc
Tomoki108 <shinonome81@gmail.com>
2024-11-26 19:40:37 +0900

    improve 378 B

--- ABC/378/B/main.go
+++ ABC/378/B/main.go
@@ -1,76 +1,152 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
 func main() {
-	var N int
-	fmt.Scan(&N)
+	defer w.Flush()
 
-	var qs, rs []int
+	N := readInt(r)
+
+	type qr struct {
+		q int // 割る数
+		r int // 余り
+	}
+
+	m := make(map[int]qr, N)
 	for i := 0; i < N; i++ {
-		var q, r int
-		fmt.Scan(&q, &r)
-		qs = append(qs, q)
-		rs = append(rs, r)
+		q, r := read2Ints(r)
+		m[i+1] = qr{q: q, r: r}
 	}
 
-	var Q int
-	fmt.Scan(&Q)
-
-	var ts, ds []int
+	Q := readInt(r)
 	for i := 0; i < Q; i++ {
-		var t, d int
-		fmt.Scan(&t, &d)
-		ts = append(ts, t)
-		ds = append(ds, d)
-	}
+		t, d := read2Ints(r) // 種類、日付
 
-	// ここまで入力
+		devider := m[t].q
+		remainder := m[t].r
 
-	type numToDevAndReminder struct {
-		numToDev int // 割る数, qi
-		reminder int // 余り, ri
-	}
-	numToDevAndReminders := make([]numToDevAndReminder, N)
-	for i := 0; i < N; i++ {
-		numToDevAndReminders[i] = numToDevAndReminder{numToDev: qs[i], reminder: rs[i]}
-	}
-	// fmt.Printf("numToDevAndReminders: %v\n", numToDevAndReminders)
-
-	type garbageInfo struct {
-		kind int // tj
-		day  int // dj
-	}
-	garbageInfos := make([]garbageInfo, Q)
-	for i := 0; i < Q; i++ {
-		garbageInfos[i] = garbageInfo{day: ds[i], kind: ts[i]}
-	}
-	// fmt.Printf("garbageInfos: %v\n", garbageInfos)
-
-	for _, garbageInfo := range garbageInfos {
-		numToDev := numToDevAndReminders[garbageInfo.kind-1].numToDev
-		remainder := numToDevAndReminders[garbageInfo.kind-1].reminder
-
-		if garbageInfo.day < remainder {
-			fmt.Println(remainder)
-			continue
-		}
-
-		remainderFromDay := garbageInfo.day % numToDev
-		if remainderFromDay == remainder {
-			fmt.Println(garbageInfo.day)
-			continue
-		}
-
-		quotientFromDay := garbageInfo.day / numToDev
-		if remainderFromDay < remainder {
-			ans := numToDev*(quotientFromDay) + remainder
-			fmt.Println(ans)
+		if d%devider == remainder {
+			fmt.Fprintln(w, d)
 		} else {
-			ans := numToDev*(quotientFromDay+1) + remainder
-			fmt.Println(ans)
+			if d%devider < remainder {
+				fmt.Fprintln(w, devider*(d/devider)+remainder)
+			} else {
+				fmt.Fprintln(w, devider*(d/devider+1)+remainder)
+			}
 		}
 	}
+}
+
+//////////////
+// Hepers  //
+/////////////
+
+// 一行に1文字のみの入力を読み込む
+func readString(r *bufio.Reader) string {
+	input, _ := r.ReadString('\n')
+
+	return strings.TrimSpace(input)
+}
+
+// 一行に1つの整数のみの入力を読み込む
+func readInt(r *bufio.Reader) int {
+	input, _ := r.ReadString('\n')
+	str := strings.TrimSpace(input)
+	i, _ := strconv.Atoi(str)
+
+	return i
+}
+
+// 一行に2つの整数のみの入力を読み込む
+func read2Ints(r *bufio.Reader) (int, int) {
+	input, _ := r.ReadString('\n')
+	strs := strings.Fields(input)
+	i1, _ := strconv.Atoi(strs[0])
+	i2, _ := strconv.Atoi(strs[1])
+
+	return i1, i2
+}
+
+// 一行に複数の文字列が入力される場合、スペース区切りで文字列を読み込む
+func readStrArr(r *bufio.Reader) []string {
+	input, _ := r.ReadString('\n')
+	return strings.Fields(input)
+}
+
+// 一行に複数の整数が入力される場合、スペース区切りで整数を読み込む
+func readIntArr(r *bufio.Reader) []int {
+	input, _ := r.ReadString('\n')
+	strs := strings.Fields(input)
+	arr := make([]int, len(strs))
+	for i, s := range strs {
+		arr[i], _ = strconv.Atoi(s)
+	}
+
+	return arr
+}
+
+// height行の文字列グリッドを読み込む
+func readGrid(r *bufio.Reader, height int) [][]string {
+	grid := make([][]string, height)
+	for i := 0; i < height; i++ {
+		grid[i] = readStrArr(r)
+	}
+
+	return grid
+}
+
+// 文字列グリッドを出力する
+func writeGrid(w *bufio.Writer, grid [][]string) {
+	for i := 0; i < len(grid); i++ {
+		fmt.Fprint(w, strings.Join(grid[i], ""), "\n")
+	}
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)を、時計回りにtime回回転させたときの座標を返す
+func rotateGridCell(n, height, width, time int) (h, w int) {
+	time = time % 4
+	switch time {
+	case 0:
+		return height, width
+	case 1:
+		return width, n - height + 1
+	case 2:
+		return n - height + 1, n - width + 1
+	case 3:
+		return n - width + 1, height
+	}
+
+	panic("can't reach here")
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)が、最も外側のマス目達を1周目としたときに何周目にあるかを返す
+func getGridCellLayer(n, h, w int) int {
+	return int(math.Min(math.Min(float64(h), float64(w)), math.Min(float64(n-h+1), float64(n-w+1))))
+}
+
+// nCrの計算 O(r)
+// (n * (n-1) ... * (n-r+1)) / r!
+func combination(n, r int) int {
+	if r > n {
+		return 0
+	}
+	if r > n/2 {
+		r = n - r // Use smaller r for efficiency
+	}
+	result := 1
+	for i := 0; i < r; i++ {
+		result *= (n - i)
+		result /= (i + 1)
+	}
+	return result
 }
