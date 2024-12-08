fa4f45945b8c31111955a78c3bfbdd40093cfa48
Tomoki108 <shinonome81@gmail.com>
2024-12-08 13:51:33 +0900

    add lib

---
+++ lib/primenum/primenum.go
@@ -0,0 +1,33 @@
+package primenum
+
+// エラストテネスの篩でN以下の素数を列挙する
+func Eratos(n int) []int {
+	if n < 2 {
+		return []int{}
+	}
+
+	// Create a boolean slice to track prime numbers
+	isPrime := make([]bool, n+1)
+	for i := 2; i <= n; i++ {
+		isPrime[i] = true
+	}
+
+	// Mark non-prime numbers
+	for i := 2; i*i <= n; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+
+	// Collect prime numbers
+	primes := []int{}
+	for i := 2; i <= n; i++ {
+		if isPrime[i] {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
