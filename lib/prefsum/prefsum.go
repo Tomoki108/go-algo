c3e62f3e9d8321b5f5d59a33f616de9522548108
Tomoki108 <shinonome81@gmail.com>
2024-12-12 17:06:09 +0900

    add lib

--- lib/prefsum/prefsum.go
+++ lib/prefsum/prefsum.go
@@ -1,5 +1,6 @@
 package prefsum
 
+// 一次元配列の累積和を返す
 func PrefixSum(sl []int) []int {
 	n := len(sl)
 	res := make([]int, n+1)
@@ -7,4 +8,46 @@ func PrefixSum(sl []int) []int {
 		res[i+1] = res[i] + sl[i]
 	}
 	return res
+}
+
+// 二次元累積和から、任意の範囲の和を求める
+// sumGridには、x, y, z方向に番兵（余分な空の一行）が含まれているものとする
+// Lx, Rxは、その軸における範囲指定 => x方向には、Rxの累積和からLx-1の累積和を引く
+func SumFrom2DPrefixSum(sumGrid [][]int, Lx, Rx, Ly, Ry int) int {
+	Lx--
+	Ly--
+
+	// 包除原理
+	result := sumGrid[Rx][Ry]
+
+	result -= sumGrid[Lx][Ry]
+	result -= sumGrid[Rx][Ly]
+
+	result += sumGrid[Lx][Ly]
+
+	return result
+}
+
+// 三次元累積和から、任意の範囲の和を求める
+// sumCubには、x, y, z方向に番兵（余分な空の一行）が含まれているものとする
+// Lx, Rxは、その軸における範囲指定 => x方向には、Rxの累積和からLx-1の累積和を引く
+func SumFrom3DPrefixSum(sumCube [][][]int, Lx, Rx, Ly, Ry, Lz, Rz int) int {
+	Lx--
+	Ly--
+	Lz--
+
+	// 包除原理
+	result := sumCube[Rx][Ry][Rz]
+
+	result -= sumCube[Lx][Ry][Rz]
+	result -= sumCube[Rx][Ly][Rz]
+	result -= sumCube[Rx][Ry][Lz]
+
+	result += sumCube[Lx][Ly][Rz]
+	result += sumCube[Lx][Ry][Lz]
+	result += sumCube[Rx][Ly][Lz]
+
+	result -= sumCube[Lx][Ly][Lz]
+
+	return result
 }
