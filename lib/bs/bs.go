6a3fd95df281f40c5c15dcef3d087a469fe855a3
Tomoki108 <shinonome81@gmail.com>
2024-12-13 14:59:58 +0900

    polish

--- lib/bs/bs.go
+++ lib/bs/bs.go
@@ -3,17 +3,19 @@ package bs
 // low, low+1, ..., highの範囲で条件を満たす最小の値を二分探索する
 // low~highは条件に対して単調増加性を満たす必要がある
 // 条件を満たす値が見つからない場合はlow-1を返す
-func IntSearch(low, high int, f func(num int) bool) int {
+func AscIntSearch(low, high int, f func(num int) bool) int {
 	for low < high {
-		mid := low + (high-low)/2 // オーバーフローを防ぐための式
+		// オーバーフローを防ぐための立式
+		// 中央値はlow側に寄る
+		mid := low + (high-low)/2
 		if f(mid) {
-			high = mid // 条件を満たす可能性がある範囲を縮める
+			high = mid // 条件を満たす場合、よりlow側の範囲を探索
 		} else {
-			low = mid + 1 // 条件を満たさない場合、次の範囲を探索
+			low = mid + 1 // 条件を満たさない場合、よりhigh側の範囲を探索
 		}
 	}
 
-	// 最後に low が条件を満たしているかを確認
+	// 最後に low(=high) が条件を満たしているかを確認
 	if f(low) {
 		return low
 	}
@@ -22,19 +24,21 @@ func IntSearch(low, high int, f func(num int) bool) int {
 }
 
 // high, high-1, ..., lowの範囲で条件を満たす最小の値を二分探索する
-// high~lowは条件に対して単調減少性を満たす必要がある
+// high~lowは条件に対して単調増加性を満たす必要がある
 // 条件を満たす値が見つからない場合はhigh+1を返す
-func RevIntSearch(low, high int, f func(num int) bool) int {
+func DescIntSearch(high, low int, f func(num int) bool) int {
 	for low < high {
-		mid := low + (high-low+1)/2 // オーバーフローを防ぐための式. 高い方を切り捨てるため +1
+		// オーバーフローを防ぐための式.
+		// 中央値はhigh側に寄る（+1しているため）
+		mid := low + (high-low+1)/2
 		if f(mid) {
-			low = mid // 条件を満たす可能性がある範囲を縮める
+			low = mid // 条件を満たす場合、よりhigh側の範囲を探索
 		} else {
-			high = mid - 1 // 条件を満たさない場合、次の範囲を探索
+			high = mid - 1 // 条件を満たさない場合、よりlow側の範囲を探索
 		}
 	}
 
-	// 最後に high が条件を満たしているかを確認
+	// 最後に high(=low) が条件を満たしているかを確認
 	if f(high) {
 		return high
 	}
