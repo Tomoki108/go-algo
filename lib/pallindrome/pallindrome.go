d365847a37eea6fd68449254a01fcbe5cc765a09
Tomoki108 <shinonome81@gmail.com>
2024-12-13 18:50:38 +0900

    add lib

---
+++ lib/pallindrome/pallindrome.go
@@ -0,0 +1,22 @@
+package pallindrome
+
+func ContainsPallindrome(ss []string, k int) bool {
+	for i := 0; i <= len(ss)-k; i++ { // K文字ずつチェック、インデックスを一個ずつずらす
+		toCheck := ss[i : i+k]
+		if IsPallindrome(toCheck) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsPallindrome(ss []string) bool {
+	for i := 0; i < len(ss)/2; i++ {
+		if ss[i] != ss[len(ss)-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
