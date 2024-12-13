1d89267fdd9c553c18d481b0446e5d63398d29d1
Tomoki108 <shinonome81@gmail.com>
2024-12-13 16:06:33 +0900

    c TLE

--- ABC/abc363/c/main.go
+++ ABC/abc363/c/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -17,11 +18,84 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	_, K := read2Ints(r)
+
+	S := readStr(r)
+	ss := strings.Split(S, "")
+
+	permitations := Permute([]string{}, ss)
+
+	done := make(map[string]bool, len(permitations))
+	ans := 0
+
+	// fmt.Printf("permitations: %v\n", permitations)
+
+Outer:
+	for _, p := range permitations {
+		if done[strings.Join(p, "")] {
+			continue
+		}
+		done[strings.Join(p, "")] = true
+
+		for i := 0; i <= len(p)-K; i++ {
+			toCheck := p[i : i+K]
+
+			reversed := make([]string, len(toCheck))
+			copy(reversed, toCheck)
+			reversed = RevSl(reversed)
+
+			// fmt.Printf("permutation: %v, toCheck: %v, reversed: %v\n", p, toCheck, reversed)
+
+			if reflect.DeepEqual(toCheck, reversed) {
+				continue Outer
+			}
+		}
+
+		ans++
+	}
+
+	fmt.Fprintln(w, ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// 順列のパターンを全列挙する
+// ex, Permute([]int{}, []int{1, 2, 3}) returns [[1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]]
+// optionsには全ての要素が異なるものを渡すこと
+func Permute[T any](current []T, options []T) [][]T {
+	var results [][]T
+
+	cc := append([]T{}, current...)
+	co := append([]T{}, options...)
+
+	if len(co) == 0 {
+		return [][]T{cc}
+	}
+
+	for i, o := range options {
+		newcc := append([]T{}, cc...)
+		newcc = append(newcc, o)
+		newco := append([]T{}, co[:i]...)
+		newco = append(newco, co[i+1:]...)
+
+		subResults := Permute(newcc, newco)
+		results = append(results, subResults...)
+	}
+
+	return results
+}
+
+func RevSl[S ~[]E, E any](s S) S {
+	lenS := len(s)
+	revS := make(S, lenS)
+	for i := 0; i < lenS; i++ {
+		revS[i] = s[lenS-1-i]
+	}
+
+	return revS
+}
 
 //////////////
 // Helpers  //
