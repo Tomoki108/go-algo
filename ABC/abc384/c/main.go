6e396e80ee26d791455e3f6b53dbc9f8c77af303
Tomoki108 <shinonome81@gmail.com>
2024-12-14 21:58:29 +0900

    c

--- ABC/abc384/c/main.go
+++ ABC/abc384/c/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,11 +18,181 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	iarr := readIntArr(r)
+	m := make(map[string]int, 5)
+
+	idx := 0
+	for i := 'A'; i <= 'E'; i++ {
+		m[string(i)] = iarr[idx]
+		idx++
+	}
+
+	// fmt.Println("hey")
+	// fmt.Println(m)
+
+	comb := map[int][][]string{
+		5: {[]string{"A", "B", "C", "D", "E"}},
+		4: {[]string{"A", "B", "C", "D"}, []string{"A", "B", "C", "E"}, []string{"A", "B", "D", "E"}, []string{"A", "C", "D", "E"}, []string{"B", "C", "D", "E"}},
+		3: {[]string{"A", "B", "C"}, []string{"A", "B", "D"}, []string{"A", "B", "E"}, []string{"A", "C", "D"}, []string{"A", "C", "E"}, []string{"A", "D", "E"}, []string{"B", "C", "D"}, []string{"B", "C", "E"}, []string{"B", "D", "E"}, []string{"C", "D", "E"}},
+		2: {[]string{"A", "B"}, []string{"A", "C"}, []string{"A", "D"}, []string{"A", "E"}, []string{"B", "C"}, []string{"B", "D"}, []string{"B", "E"}, []string{"C", "D"}, []string{"C", "E"}, []string{"D", "E"}},
+		1: {[]string{"A"}, []string{"B"}, []string{"C"}, []string{"D"}, []string{"E"}},
+	}
+
+	var ps [][]string
+	for i := 1; i <= 5; i++ {
+		ps = append(ps, comb[i]...)
+	}
+
+	// var ps [][]string
+	// for i := 5; i >= 1; i-- {
+	// 	optionsSl := comb[i]
+	// 	for _, options := range optionsSl {
+
+	// 		// fmt.Println("hey")
+
+	// 		result := Permute([]string{}, options)
+
+	// 		// fmt.Println(result)
+
+	// 		ps = append(ps, result...)
+	// 	}
+
+	// }
+
+	sort.Slice(ps, func(i, j int) bool {
+		p1 := ps[i]
+		p2 := ps[j]
+
+		score1 := 0
+		score2 := 0
+		for i := 0; i < len(p1); i++ {
+			char := p1[i]
+			score1 += m[char]
+		}
+		for i := 0; i < len(p2); i++ {
+			char := p2[i]
+			score2 += m[char]
+		}
+
+		length := min(len(p1), len(p2))
+		if score1 == score2 {
+			for i := 0; i < length; i++ {
+				if i == length {
+					if len(p1) < len(p2) {
+						return true
+					} else if len(p1) > len(p2) {
+						return false
+					}
+				}
+
+				char := p1[i]
+				char2 := p2[i]
+
+				if char < char2 {
+					return true
+				} else if char > char2 {
+					return false
+				}
+			}
+		}
+
+		return score1 > score2
+	})
+
+	for _, p := range ps {
+		fmt.Fprintln(w, strings.Join(p, ""))
+	}
+
+	// // fmt.Println(pss[0])
+	// // return
+
+	// for _, ps := range pss {
+	// 	sort.Slice(ps, func(i, j int) bool {
+	// 		p1 := ps[i]
+	// 		p2 := ps[j]
+
+	// 		score1 := 0
+	// 		score2 := 0
+
+	// 		length := len(p1)
+
+	// 		for i := 0; i < length; i++ {
+	// 			char := p1[i]
+	// 			score1 += m[char]
+
+	// 			char2 := p2[i]
+	// 			score2 += m[char2]
+	// 		}
+
+	// 		return score1 > score2
+	// 	})
+
+	// 	for _, p := range ps {
+	// 		fmt.Println(w, strings.Join(p, ""), "\n")
+	// 	}
+	// }
+
 }
 
 //////////////
 // Libs    //
 /////////////
+
+func Permute[T comparable](current []T, options []T) [][]T {
+	var results [][]T
+
+	cc := append([]T{}, current...)
+	co := append([]T{}, options...)
+
+	if len(co) == 0 {
+		return [][]T{cc}
+	}
+
+	var lastO T
+	for i, o := range options {
+		if o == lastO {
+			continue
+		}
+		lastO = o
+
+		newcc := append([]T{}, cc...)
+		newcc = append(newcc, o)
+		newco := append([]T{}, co[:i]...)
+		newco = append(newco, co[i+1:]...)
+
+		subResults := Permute(newcc, newco)
+		results = append(results, subResults...)
+	}
+
+	return results
+}
+
+// 要素数 len(options) で、i番目の要素が options[i] であるような順列のパターンを全列挙する
+// options[i]に重複した要素が含まれていても、あらかじめソートしておけば重複パターンは除かれる
+func Permute2[T comparable](current []T, options [][]T) [][]T {
+	var results [][]T
+
+	if len(current) == len(options) {
+		results = append(results, current)
+		return results
+	}
+
+	nextVals := options[len(current)]
+	var lastV T
+	for _, v := range nextVals {
+		if v == lastV {
+			continue
+		}
+		lastV = v
+
+		copyCurrent := append([]T{}, current...)
+		copyCurrent = append(copyCurrent, v)
+		subResults := Permute2(copyCurrent, options)
+		results = append(results, subResults...)
+	}
+
+	return results
+}
 
 //////////////
 // Helpers  //
