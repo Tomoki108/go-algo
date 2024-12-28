5b644cd12dfc279d5d49f8bc8730a3d101da9c51
Tomoki108 <shinonome81@gmail.com>
2024-12-28 21:07:45 +0900

    b

--- ABC/abc386/b/main.go
+++ ABC/abc386/b/main.go
@@ -20,11 +20,65 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	S := readStr(r)
+	Ss := strings.Split(S, "")
+
+	css := RunLength(Ss)
+
+	ans := 0
+	for _, cs := range css {
+		num, c := SplitRLStr(cs)
+
+		if c == "0" {
+			quotient := num / 2
+			rem := num % 2
+
+			ans += quotient + rem
+		} else {
+			ans += num
+		}
+	}
+
+	fmt.Fprintln(w, ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+var Delimiter = "_"
+
+// ランレングス圧縮を行う。[]"数+delimiter+文字種"を返す。
+func RunLength(sl []string) []string {
+	comp := make([]string, 0, len(sl))
+	if len(sl) == 0 {
+		return comp
+	}
+
+	lastChar := sl[0]
+	currentLen := 0
+	for i := 0; i < len(sl); i++ {
+		s := sl[i]
+		if s == lastChar {
+			currentLen++
+		} else {
+			comp = append(comp, strconv.Itoa(currentLen)+Delimiter+lastChar)
+			lastChar = s
+			currentLen = 1
+		}
+	}
+	comp = append(comp, strconv.Itoa(currentLen)+Delimiter+lastChar) // 最後の一文字
+
+	return comp
+}
+
+// "数+delimiter+文字種"を分割して数と文字種を返す
+func SplitRLStr(s string) (int, string) {
+	strs := strings.Split(s, Delimiter)
+	num, _ := strconv.Atoi(strs[0])
+
+	return num, strs[1]
+}
 
 //////////////
 // Helpers  //
