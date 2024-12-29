bb8b168322703b5a72cd1646ff79b40e210dc8a6
Tomoki108 <shinonome81@gmail.com>
2024-12-29 21:24:59 +0900

    polish

--- lib/compress/compress.go
+++ lib/compress/compress.go
@@ -5,10 +5,8 @@ import (
 	"strings"
 )
 
-var Delimiter = "_"
-
 // ランレングス圧縮を行う。[]"数+delimiter+文字種"を返す。
-func RunLength(sl []string) []string {
+func RunLength(sl []string, delimiter string) []string {
 	comp := make([]string, 0, len(sl))
 	if len(sl) == 0 {
 		return comp
@@ -21,19 +19,19 @@ func RunLength(sl []string) []string {
 		if s == lastChar {
 			currentLen++
 		} else {
-			comp = append(comp, strconv.Itoa(currentLen)+Delimiter+lastChar)
+			comp = append(comp, strconv.Itoa(currentLen)+delimiter+lastChar)
 			lastChar = s
 			currentLen = 1
 		}
 	}
-	comp = append(comp, strconv.Itoa(currentLen)+Delimiter+lastChar) // 最後の一文字
+	comp = append(comp, strconv.Itoa(currentLen)+delimiter+lastChar) // 最後の一文字
 
 	return comp
 }
 
 // "数+delimiter+文字種"を分割して数と文字種を返す
-func SplitRLStr(s string) (int, string) {
-	strs := strings.Split(s, Delimiter)
+func SplitRLStr(s, delimiter string) (int, string) {
+	strs := strings.Split(s, delimiter)
 	num, _ := strconv.Atoi(strs[0])
 
 	return num, strs[1]
