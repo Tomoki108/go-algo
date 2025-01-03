476190ba4e57999f657f15d93952defe63c6023b
Tomoki108 <shinonome81@gmail.com>
2025-01-03 15:18:56 +0900

    add lib

--- lib/math/math.go
+++ lib/math/math.go
@@ -1,20 +1,10 @@
 package math
 
-// O(log(exp))
-// 繰り返し二乗法で x^y を計算する関数
-func Pow(base, exp int) int {
-	// 繰り返し二乗法
-	// 2^8 = 4^2^2
-	// 2^9 = 4^2^2 * 2
-	// この性質を利用して、基数を2乗しつつ指数を1/2にしていく
-
-	result := 1
-	for exp > 0 {
-		if exp%2 == 1 {
-			result *= base
-		}
-		base *= base
-		exp /= 2
+func GetDigists(num int) int {
+	digits := 0
+	for num > 0 {
+		num /= 10
+		digits++
 	}
-	return result
+	return digits
 }
