3b33ef3b44ac38b5e66912ccacdc9d7fb272407f
Tomoki108 <shinonome81@gmail.com>
2025-01-03 21:52:36 +0900

    comment

--- lib/tree/tree_set.go
+++ lib/tree/tree_set.go
@@ -33,3 +33,4 @@ func Last[T any](set *treeset.Set) T {
 }
 
 // ちなみに、c++のmutisetはredblacktreeを使い、keyに値、valueに回数を持てば実現出来る
+// https://qiita.com/hima398/items/26e86d8dc3ab0b7c5e12#%E6%AE%8B%E3%81%A3%E3%81%A6%E3%81%84%E3%82%8B%E8%AA%B2%E9%A1%8C
