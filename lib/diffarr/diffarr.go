1d73ca2b9391ce76dd6629d4104a40be35271fb4
Tomoki108 <shinonome81@gmail.com>
2025-01-12 22:05:47 +0900

    add liv

---
+++ lib/diffarr/diffarr.go
@@ -0,0 +1,24 @@
+package diffarr
+
+// O(n)
+// intスライスの差分配列を返す
+func DiffArray(sl []int) []int {
+	res := make([]int, 0, len(sl))
+	res = append(res, sl[0])
+	for i := 1; i < len(sl); i++ {
+		res = append(res, sl[i]-sl[i-1])
+	}
+	return res
+}
+
+// O(1)
+// 差分配列への区間更新を行う。
+// 更新後に累積和をとっていくと、各インデックスの値が求まる。所謂imos法
+func RangeUpdateDiffArray(sl []int, l, r, x int) {
+	if l < len(sl) {
+		sl[l] += x
+	}
+	if r < len(sl) {
+		sl[r] -= x
+	}
+}
