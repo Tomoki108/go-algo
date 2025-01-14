87c307c8cce93f5fe479bf1b28e811267c47e89e
Tomoki108 <shinonome81@gmail.com>
2025-01-14 21:27:35 +0900

    a

--- ABC/abc339/a/main.go
+++ ABC/abc339/a/main.go
@@ -21,11 +21,34 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	S := readStr(r)
+	Ss := strings.Split(S, "")
+
+	ans := make([]string, 0)
+	for i := len(Ss) - 1; i >= 0; i-- {
+		if Ss[i] == "." {
+			break
+		}
+
+		ans = append(ans, Ss[i])
+	}
+
+	SlRev(ans)
+
+	fmt.Fprintln(w, strings.Join(ans, ""))
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// O(n)
+// slices.Reverce() と同じ（Goのバージョンが1.21以前だと使えないため）
+func SlRev[S ~[]E, E any](s S) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
 
 //////////////
 // Helpers  //
