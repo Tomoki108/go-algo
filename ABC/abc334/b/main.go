f35a15a047ce0e8739cffb96b3b738e6d037925e
Tomoki108 <shinonome81@gmail.com>
2025-01-17 17:47:41 +0900

    b

--- ABC/abc334/b/main.go
+++ ABC/abc334/b/main.go
@@ -21,11 +21,33 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	iarr := readIntArr(r)
+	A, M, L, R := iarr[0], iarr[1], iarr[2], iarr[3]
+
+	L -= A
+	R -= A
+
+	mostRightK := (R - Mod(R, M)) / M
+	mostLeftK := (L - 1 - Mod(L-1, M)) / M
+
+	ans := mostRightK - mostLeftK
+	fmt.Fprintln(w, ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// a割るbの、数学における剰余を返す。
+// a = b * Quotient + RemainderとなるRemainderを返す（Quotientは負でもよく、Remainderは常に0以上という制約がある）
+// goのa%bだと、|a|割るbの剰余にaの符号をつけて返すため、負の数が含まれる場合数学上の剰余とは異なる。
+func Mod(a, b int) int {
+	r := a % b
+	if r < 0 {
+		r += b
+	}
+	return r
+}
 
 //////////////
 // Helpers  //
