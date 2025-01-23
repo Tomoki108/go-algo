1b9438dd301056639566230e2e2a4346b3a67295
Tomoki108 <shinonome81@gmail.com>
2025-01-23 18:11:32 +0900

    ac d

--- ABC/abc324/d/main.go
+++ ABC/abc324/d/main.go
@@ -29,36 +29,33 @@ func main() {
 		return Ss[i] > Ss[j]
 	})
 
+	originalKey := strings.Join(Ss, "")
+
 	maxNum := atoi(strings.Join(Ss, ""))
 	maxNumRoute := int(math.Sqrt(float64(maxNum))) + 1
 
 	dump("%d\n", maxNum)
 	dump("%d\n", maxNumRoute)
 
-	// 平方数をstringの配列に分割=>ソート=>結合したものをキーにして、その数をカウントする
-	sqNumMap := make(map[string]int, maxNumRoute)
+	ans := 0
 	for i := 0; i <= maxNumRoute; i++ {
 		sqNum := i * i
 
+		// 平方数をstringの配列に分割=>ソート=>結合したものをキーにして、オリジナルのキーと一致するかを判定
 		numStr := itoa(sqNum)
 		numStrs := strings.Split(numStr, "")
 		sort.Slice(numStrs, func(i, j int) bool {
 			return numStrs[i] > numStrs[j]
 		})
-
 		for len(numStrs) < len(Ss) {
 			numStrs = append(numStrs, "0")
 		}
-
 		key := strings.Join(numStrs, "")
 
-		sqNumMap[key]++
+		if key == originalKey {
+			ans++
+		}
 	}
-
-	dump("%v\n", sqNumMap)
-
-	key := strings.Join(Ss, "")
-	ans := sqNumMap[key]
 
 	fmt.Fprintln(w, ans)
 }
@@ -66,65 +63,6 @@ func main() {
 //////////////
 // Libs    //
 /////////////
-
-// NOTE:
-// next := true; for next { some(sl); next = NextPermutation(sl); } で使う
-//
-// O(len(sl)*len(sl)!)
-// sl の要素を並び替えて、次の辞書順の順列にする
-func NextPermutation[T ~int | ~string](sl []T) bool {
-	n := len(sl)
-	i := n - 2
-
-	// Step1: 右から左に探索して、「スイッチポイント」を見つける:
-	// 　「スイッチポイント」とは、右から見て初めて「リストの値が減少する場所」です。
-	// 　例: [1, 2, 3, 6, 5, 4] の場合、3 がスイッチポイント。
-	for i >= 0 && sl[i] >= sl[i+1] {
-		i--
-	}
-
-	//　スイッチポイントが見つからない場合、最後の順列に到達しています。
-	if i < 0 {
-		return false
-	}
-
-	// Step2: スイッチポイントの右側の要素から、スイッチポイントより少しだけ大きい値を見つけ、交換します。
-	// 　例: 3 を右側で最小の大きい値 4 と交換。
-	j := n - 1
-	for sl[j] <= sl[i] {
-		j--
-	}
-	sl[i], sl[j] = sl[j], sl[i]
-
-	// Step3: スイッチポイントの右側を反転して、辞書順に次の順列を作ります。
-	// 　例: [1, 2, 4, 6, 5, 3] → [1, 2, 4, 3, 5, 6]。
-	reverse(sl[i+1:])
-	return true
-}
-
-func reverse[T ~int | ~string](sl []T) {
-	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
-		sl[i], sl[j] = sl[j], sl[i]
-	}
-}
-
-// O(√n)
-// 素因数分解を行い、素因数=>指数のmapを返す（keyは昇順）
-func PrimeFactorization(n int) map[int]int {
-	pf := make(map[int]int)
-	// 因数候補は√nまででいい。
-	// √nより大きい数で割った場合、商は√nより小さい数になるため、√n以下の検証時にその割り算は済んでいる。
-	for factor := 2; factor*factor <= n; factor++ {
-		for n%factor == 0 {
-			pf[factor]++
-			n /= factor
-		}
-	}
-	if n > 1 {
-		pf[n]++
-	}
-	return pf
-}
 
 //////////////
 // Helpers //
