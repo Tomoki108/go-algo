2f93e8b8d3dd2e902f09cb07b838da929e617849
Tomoki108 <shinonome81@gmail.com>
2025-01-24 16:43:55 +0900

    WA

--- ABC/abc322/d/main.go
+++ ABC/abc322/d/main.go
@@ -21,11 +21,172 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	grid1 := readGrid(r, 4)
+	grid2 := readGrid(r, 4)
+	grid3 := readGrid(r, 4)
+
+	getParts := func(grid [][]string) [][2]int {
+		var pFirst *[2]int
+		var pSlice [][2]int
+		for i := 0; i < 4; i++ {
+			for j := 0; j < 4; j++ {
+				if grid1[i][j] == "#" {
+					if pFirst == nil {
+						pFirst = &[2]int{i, j}
+					} else {
+						pSlice = append(pSlice, [2]int{i - pFirst[0], j - pFirst[1]})
+					}
+				}
+			}
+		}
+		return pSlice
+	}
+
+	p1 := getParts(grid1)
+	p2 := getParts(grid2)
+	p3 := getParts(grid3)
+	partsSl := [3][][2]int{p1, p2, p3}
+
+	var dfs func(mostLeftUp [2]int, partsSl [3][][2]int, partsIdx int, grid [][]string) bool
+	dfs = func(mostLeftUp [2]int, partsSl [3][][2]int, partsIdx int, grid [][]string) bool {
+		if partsIdx >= 3 {
+			panic("can't reach here")
+		}
+
+		parts := partsSl[partsIdx]
+		newParts := make([][2]int, 0, len(parts))
+		for _, part := range parts {
+			newParts = append(newParts, [2]int{part[0] + mostLeftUp[0], part[1] + mostLeftUp[1]})
+		}
+
+	Outer:
+		for i := 0; i <= 3; i++ {
+			cgrid := CopyGrid(grid)
+
+			for _, part := range newParts {
+				nh, nw := RotateSquareGridCell(4, part[0], part[1], i)
+				c := Coordinate{nh, nw}
+				if !c.IsValid(4, 4) || grid[c.h][c.w] == "#" {
+					continue Outer
+				}
+				cgrid[c.h][c.w] = "#"
+			}
+
+			var newMostLeftUp *[2]int
+			for h := 0; h < 4; h++ {
+				for w := 0; w < 4; w++ {
+					if cgrid[h][w] == "." {
+						newMostLeftUp = &[2]int{h, w}
+					}
+				}
+			}
+			if newMostLeftUp == nil {
+				return true
+			}
+
+			newPartsIdx := partsIdx + 1
+			return dfs(*newMostLeftUp, partsSl, newPartsIdx, cgrid)
+		}
+
+		return false
+	}
+
+	perm := []int{0, 1, 2}
+	next := true
+	for next {
+		newPartsSl := [3][][2]int{partsSl[perm[0]], partsSl[perm[1]], partsSl[perm[2]]}
+		if dfs([2]int{0, 0}, newPartsSl, 0, createGrid(4, 4, ".")) {
+			fmt.Println("Yes")
+			return
+		}
+		next = NextPermutation(perm)
+	}
+
+	fmt.Println("No")
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// O(H*W)
+// T型グリッドのコピーを作成する
+func CopyGrid[T any](grid [][]T) [][]T {
+	H := len(grid)
+	W := len(grid[0])
+	res := make([][]T, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]T, W)
+		copy(res[i], grid[i])
+	}
+	return res
+}
+
+// 一辺がnの正方形グリッドのマス目(hight, width)を、時計回りにtime回回転させたときの座標を返す
+func RotateSquareGridCell(n, height, width, time int) (h, w int) {
+	time = time % 4
+	switch time {
+	case 0:
+		return height, width
+	case 1:
+		return width, n - height + 1
+	case 2:
+		return n - height + 1, n - width + 1
+	case 3:
+		return n - width + 1, height
+	}
+
+	panic("can't reach here")
+}
+
+type Coordinate struct {
+	h, w int // 0-indexed
+}
+
+func (c Coordinate) IsValid(H, W int) bool {
+	return 0 <= c.h && c.h < H && 0 <= c.w && c.w < W
+}
+
+// NOTE:
+// next := true; for next { some(sl); next = NextPermutation(sl); } で使う
+//
+// O(len(sl)*len(sl)!)
+// sl の要素を並び替えて、次の辞書順の順列にする
+func NextPermutation[T ~int | ~string](sl []T) bool {
+	n := len(sl)
+	i := n - 2
+
+	// Step1: 右から左に探索して、「スイッチポイント」を見つける:
+	// 　「スイッチポイント」とは、右から見て初めて「リストの値が減少する場所」です。
+	// 　例: [1, 2, 3, 6, 5, 4] の場合、3 がスイッチポイント。
+	for i >= 0 && sl[i] >= sl[i+1] {
+		i--
+	}
+
+	//　スイッチポイントが見つからない場合、最後の順列に到達しています。
+	if i < 0 {
+		return false
+	}
+
+	// Step2: スイッチポイントの右側の要素から、スイッチポイントより少しだけ大きい値を見つけ、交換します。
+	// 　例: 3 を右側で最小の大きい値 4 と交換。
+	j := n - 1
+	for sl[j] <= sl[i] {
+		j--
+	}
+	sl[i], sl[j] = sl[j], sl[i]
+
+	// Step3: スイッチポイントの右側を反転して、辞書順に次の順列を作ります。
+	// 　例: [1, 2, 4, 6, 5, 3] → [1, 2, 4, 3, 5, 6]。
+	reverse(sl[i+1:])
+	return true
+}
+
+func reverse[T ~int | ~string](sl []T) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
 
 //////////////
 // Helpers //
