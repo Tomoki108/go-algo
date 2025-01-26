1b4b7b17af2863561111507505d3be639d139ebc
Tomoki108 <shinonome81@gmail.com>
2025-01-26 23:23:39 +0900

    WA

--- ABC/abc319/d/main.go
+++ ABC/abc319/d/main.go
@@ -21,11 +21,96 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N, M := read2Ints(r)
+	Ls := readIntArr(r)
+
+	lspsum := PrefixSum(Ls)
+	maxWindowSize := lspsum[len(lspsum)-1] + N - 1
+
+	ans := AscIntSearch(1, maxWindowSize, func(windowSize int) bool {
+		currentSize := -1
+		rows := 1
+		for i := 0; i < N; i++ {
+			currentSize += 1 + Ls[i]
+
+			if currentSize > windowSize {
+				currentSize = Ls[i]
+				rows++
+			}
+		}
+
+		if rows > M {
+			return false
+		} else {
+			return true
+		}
+	})
+
+	fmt.Fprintln(w, ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// O(n)
+// 一次元累積和を返す（index0には0を入れる。）
+func PrefixSum(sl []int) []int {
+	n := len(sl)
+	res := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		res[i+1] = res[i] + sl[i]
+	}
+	return res
+}
+
+// O(log(high-low))
+// low, low+1, ..., highの範囲で条件を満たす最小の値を二分探索する
+// low~highは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はlow-1を返す
+func AscIntSearch(low, high int, f func(num int) bool) int {
+	for low < high {
+		// オーバーフローを防ぐための立式
+		// 中央値はlow側に寄る
+		mid := low + (high-low)/2
+		if f(mid) {
+			high = mid // 条件を満たす場合、よりlow側の範囲を探索
+		} else {
+			low = mid + 1 // 条件を満たさない場合、よりhigh側の範囲を探索
+		}
+	}
+
+	// 最後に low(=high) が条件を満たしているかを確認
+	if f(low) {
+		return low
+	}
+
+	return low - 1 // 条件を満たす値が見つからない場合
+}
+
+// O(log(high-low))
+// high, high-1, ..., lowの範囲で条件を満たす最小の値を二分探索する
+// high~lowは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はhigh+1を返す
+func DescIntSearch(high, low int, f func(num int) bool) int {
+	for low < high {
+		// オーバーフローを防ぐための式.
+		// 中央値はhigh側に寄る（+1しているため）
+		mid := low + (high-low+1)/2
+		if f(mid) {
+			low = mid // 条件を満たす場合、よりhigh側の範囲を探索
+		} else {
+			high = mid - 1 // 条件を満たさない場合、よりlow側の範囲を探索
+		}
+	}
+
+	// 最後に high(=low) が条件を満たしているかを確認
+	if f(high) {
+		return high
+	}
+
+	return high + 1 // 条件を満たす値が見つからない場合
+}
 
 //////////////
 // Helpers //
