b5807d7237f260d91dd0e552a7f0a53b3f195545
Tomoki108 <shinonome81@gmail.com>
2025-01-26 22:26:12 +0900

    ac c

--- ABC/abc319/c/main.go
+++ ABC/abc319/c/main.go
@@ -21,11 +21,124 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	grid := readIntGrid(r, 3)
+
+	m := map[int][2]int{
+		1: {0, 0}, // h, w
+		2: {0, 1},
+		3: {0, 2},
+		4: {1, 0},
+		5: {1, 1},
+		6: {1, 2},
+		7: {2, 0},
+		8: {2, 1},
+		9: {2, 2},
+	}
+
+	disappointed := 0
+	notDisappointed := 0
+
+	perm := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	next := true
+	for next {
+		rows := make([][]int, 3)
+		cols := make([][]int, 3)
+		leftCross := make([]int, 0, 3)
+		rightCross := make([]int, 0, 3)
+
+		isDisappointed := false
+		for _, cellNo := range perm {
+			h, w := m[cellNo][0], m[cellNo][1]
+			num := grid[h][w]
+
+			rows[h] = append(rows[h], num)
+			if len(rows[h]) == 2 && rows[h][0] == num {
+				isDisappointed = true
+				disappointed++
+				break
+			}
+
+			cols[w] = append(cols[w], num)
+			if len(cols[w]) == 2 && cols[w][0] == num {
+				isDisappointed = true
+				disappointed++
+				break
+			}
+
+			if h == w {
+				leftCross = append(leftCross, num)
+				if len(leftCross) == 2 && leftCross[0] == num {
+					isDisappointed = true
+					disappointed++
+					break
+				}
+			}
+			if h+w == 2 {
+				rightCross = append(rightCross, num)
+				if len(rightCross) == 2 && rightCross[0] == num {
+					isDisappointed = true
+					disappointed++
+					break
+				}
+			}
+		}
+
+		if !isDisappointed {
+			notDisappointed++
+		}
+
+		next = NextPermutation(perm)
+	}
+
+	ans := float64(notDisappointed) / float64(disappointed+notDisappointed)
+	fmt.Println(ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// NOTE:
+// next := true; for next { some(sl); next = NextPermutation(sl); } で使う
+//
+// O(len(sl)*len(sl)!)
+// sl の要素を並び替えて、次の辞書順の順列にする
+func NextPermutation[T ~int | ~string](sl []T) bool {
+	n := len(sl)
+	i := n - 2
+
+	// Step1: 右から左に探索して、「スイッチポイント」を見つける:
+	// 　「スイッチポイント」とは、右から見て初めて「リストの値が減少する場所」です。
+	// 　例: [1, 2, 3, 6, 5, 4] の場合、3 がスイッチポイント。
+	for i >= 0 && sl[i] >= sl[i+1] {
+		i--
+	}
+
+	//　スイッチポイントが見つからない場合、最後の順列に到達しています。
+	if i < 0 {
+		return false
+	}
+
+	// Step2: スイッチポイントの右側の要素から、スイッチポイントより少しだけ大きい値を見つけ、交換します。
+	// 　例: 3 を右側で最小の大きい値 4 と交換。
+	j := n - 1
+	for sl[j] <= sl[i] {
+		j--
+	}
+	sl[i], sl[j] = sl[j], sl[i]
+
+	// Step3: スイッチポイントの右側を反転して、辞書順に次の順列を作ります。
+	// 　例: [1, 2, 4, 6, 5, 3] → [1, 2, 4, 3, 5, 6]。
+	reverse(sl[i+1:])
+	return true
+}
+
+func reverse[T ~int | ~string](sl []T) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
 
 //////////////
 // Helpers //
