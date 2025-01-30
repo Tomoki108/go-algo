8e53ef763963e48f99971242d77b153d91d22986
Tomoki108 <shinonome81@gmail.com>
2025-01-30 13:50:50 +0900

    AC C

--- ABC/abc312/c/main.go
+++ ABC/abc312/c/main.go
@@ -25,43 +25,28 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
-	_, M := read2Ints(r)
+	N, M := read2Ints(r)
 	As := readIntArr(r)
 	Bs := readIntArr(r)
 
-	sort.Ints(As)
+	sort.Slice(As, func(i, j int) bool {
+		return As[i] > As[j]
+	})
 	sort.Ints(Bs)
 
-	// sort.Slice(Bs, func(i, j int) bool {
-	// 	return Bs[i] > Bs[j]
-	// })
-
-	ans := INT_MAX
-	for i, a := range As {
-		// if i < N-1 && a == As[i+1] {
-		// 	continue
-		// }
-
-		idx := sort.Search(len(Bs), func(j int) bool {
-			return Bs[j] >= a
+	ans := AscIntSearch(0, pow(10, 9)+1, func(num int) bool {
+		aIdx := sort.Search(len(As), func(i int) bool {
+			return As[i] <= num
 		})
-		if idx == len(Bs) {
-			break
-		}
+		sellable := N - aIdx
 
-		affordable := M - idx
-		sellable := i + 1
+		bIdx := sort.Search(len(Bs), func(i int) bool {
+			return Bs[i] >= num
+		})
+		affordable := M - bIdx
 
-		if sellable >= affordable {
-			ans = a
-			break
-		}
-	}
-
-	if ans == INT_MAX {
-		fmt.Fprintln(w, Bs[M-1]+1)
-		return
-	}
+		return sellable >= affordable
+	})
 
 	fmt.Fprintln(w, ans)
 }
@@ -69,6 +54,32 @@ func main() {
 //////////////
 // Libs    //
 /////////////
+
+// O(log(high-low))
+// low, low+1, ..., highの範囲で条件を満たす最小の値を二分探索する
+// low~highは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はlow-1を返す
+func AscIntSearch(low, high int, f func(num int) bool) int {
+	initialLow := low
+
+	for low < high {
+		// オーバーフローを防ぐための立式
+		// 中央値はlow側に寄る
+		mid := low + (high-low)/2
+		if f(mid) {
+			high = mid // 条件を満たす場合、よりlow側の範囲を探索
+		} else {
+			low = mid + 1 // 条件を満たさない場合、よりhigh側の範囲を探索
+		}
+	}
+
+	// 最後に low(=high) が条件を満たしているかを確認
+	if f(low) {
+		return low
+	}
+
+	return initialLow - 1 // 条件を満たす値が見つからない場合
+}
 
 //////////////
 // Helpers //
