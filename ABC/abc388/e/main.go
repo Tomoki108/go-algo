894a2acb5b41406984b1c1a58cf5bc56086ea7e3
Tomoki108 <shinonome81@gmail.com>
2025-02-03 21:33:08 +0900

    ac e

--- ABC/abc388/e/main.go
+++ ABC/abc388/e/main.go
@@ -21,11 +21,50 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N := readInt(r)
+	As := readIntArr(r)
+
+	ans := DescIntSearch(N/2, 0, func(k int) bool {
+		for i := 0; i < k; i++ {
+			top := As[i]
+			bottom := As[N-k+i]
+			if top*2 > bottom {
+				return false
+			}
+		}
+		return true
+	})
+
+	fmt.Println(ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// O(log(high-low))
+// high, high-1, ..., lowの範囲で条件を満たす最大の値を二分探索する
+// high~lowは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はhigh+1を返す
+func DescIntSearch(high, low int, f func(num int) bool) int {
+	for low < high {
+		// オーバーフローを防ぐための式.
+		// 中央値はhigh側に寄る（+1しているため）
+		mid := low + (high-low+1)/2
+		if f(mid) {
+			low = mid // 条件を満たす場合、よりhigh側の範囲を探索
+		} else {
+			high = mid - 1 // 条件を満たさない場合、よりlow側の範囲を探索
+		}
+	}
+
+	// 最後に high(=low) が条件を満たしているかを確認
+	if f(high) {
+		return high
+	}
+
+	return high + 1 // 条件を満たす値が見つからない場合
+}
 
 //////////////
 // Helpers  //
