f6e09111346ae005cba367a07b37bf23f6187065
Tomoki108 <shinonome81@gmail.com>
2025-02-03 18:37:50 +0900

    WA

--- ABC/abc380/e/main.go
+++ ABC/abc380/e/main.go
@@ -24,11 +24,139 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N, Q := read2Ints(r)
+
+	uf := NewUnionFind(N)
+
+	rootsColor := make([]int, N)
+	for i := 0; i < N; i++ {
+		rootsColor[i] = i
+	}
+	colorRoots := make(map[int]int)
+	for i := 0; i < N; i++ {
+		colorRoots[i] = i
+	}
+
+	for i := 0; i < Q; i++ {
+		iarr := readIntArr(r)
+		q := iarr[0]
+
+		switch q {
+		case 1:
+			x, c := iarr[1], iarr[2]
+			x--
+			c--
+
+			root := uf.Find(x)
+			prevColor := rootsColor[root]
+			if x == root {
+				colorRoots[prevColor] = -1
+			}
+			rootsColor[root] = c
+
+			if x+1 < N && !uf.IsSameRoot(x, x+1) {
+				rightColor := rootsColor[uf.Find(x+1)]
+				if rightColor == c {
+					uf.Union(x, x+1)
+				}
+				colorRoots[c] = uf.Find(x)
+			}
+
+			if x-1 >= 0 && !uf.IsSameRoot(x, x-1) {
+				leftColor := rootsColor[uf.Find(x-1)]
+				if leftColor == c {
+					uf.Union(x, x-1)
+				}
+				colorRoots[c] = uf.Find(x)
+			}
+		case 2:
+			c := iarr[1]
+			c--
+
+			root := colorRoots[c]
+			if root == -1 {
+				fmt.Fprintln(w, 0)
+			} else {
+				count := uf.GroupSize(root)
+				fmt.Fprintln(w, count)
+			}
+		}
+	}
 }
 
 //////////////
 // Libs    //
 /////////////
+
+type UnionFind struct {
+	parent []int // len(parent)分のノードを考え、各ノードの親を記録している
+	size   []int // そのノードを頂点とする部分木の頂点数
+}
+
+func NewUnionFind(size int) *UnionFind {
+	parent := make([]int, size)
+	s := make([]int, size)
+	for i := range parent {
+		parent[i] = i
+		s[i] = 1
+	}
+	return &UnionFind{parent, s}
+}
+
+// O(α(N))　※定数時間。α(N)はアッカーマン関数の逆関数
+// xの親を見つける
+func (uf *UnionFind) Find(xIdx int) int {
+	if uf.parent[xIdx] != xIdx {
+		uf.parent[xIdx] = uf.Find(uf.parent[xIdx]) // 経路圧縮
+	}
+	return uf.parent[xIdx]
+}
+
+// O(α(N))
+// xとyを同じグループに統合する（サイズが大きい方に統合）
+func (uf *UnionFind) Union(xIdx, yIdx int) {
+	rootX := uf.Find(xIdx)
+	rootY := uf.Find(yIdx)
+
+	if rootX != rootY {
+		if uf.size[rootX] < uf.size[rootY] {
+			uf.parent[rootX] = rootY
+			uf.size[rootY] += uf.size[rootX]
+		} else if uf.size[rootX] > uf.size[rootY] {
+			uf.parent[rootY] = rootX
+			uf.size[rootX] += uf.size[rootY]
+		} else {
+			uf.parent[rootY] = rootX
+			uf.size[rootX] += uf.size[rootY]
+		}
+	}
+}
+
+// O(1)
+func (uf *UnionFind) IsRoot(xIdx int) bool {
+	return uf.parent[xIdx] == xIdx
+}
+
+// O(α(N))
+func (uf *UnionFind) IsSameRoot(xIdx, yIdx int) bool {
+	return uf.Find(xIdx) == uf.Find(yIdx)
+}
+
+// O(N)
+func (uf *UnionFind) CountRoots() int {
+	count := 0
+	for i := range uf.parent {
+		if uf.parent[i] == i {
+			count++
+		}
+	}
+	return count
+}
+
+// O(α(N))
+func (uf *UnionFind) GroupSize(xIdx int) int {
+	return uf.size[uf.Find(xIdx)]
+}
 
 //////////////
 // Helpers //
