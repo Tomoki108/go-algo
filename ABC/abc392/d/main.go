df2e281242837705151188f83050f75c49068390
Tomoki108 <shinonome81@gmail.com>
2025-02-08 21:48:32 +0900

    WA

--- ABC/abc392/d/main.go
+++ ABC/abc392/d/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"math"
 	"os"
@@ -24,11 +25,159 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N := readInt(r)
+
+	numPecentages := make(map[int]*Heap[hItem]) // 各数字の出現割合
+
+	for i := 0; i < N; i++ {
+		iarr := readIntArr(r)
+		K := iarr[0]
+
+		numCount := make(map[int]int, K)
+		for j := 1; j < K; j++ {
+
+			numCount[iarr[j]]++
+		}
+
+		for num, cnt := range numCount {
+			if numPecentages[num] == nil {
+				numPecentages[num] = &Heap[hItem]{}
+			}
+			numPecentages[num].PushItem(hItem{cnt: cnt, K: K})
+		}
+	}
+
+	ans := float64(-1)
+	for num, pernumPecentages := range numPecentages {
+		if pernumPecentages.Len() < 2 {
+			continue
+		}
+
+		item1 := pernumPecentages.PopItem()
+		item2 := pernumPecentages.PopItem()
+
+		dump("num: %v\n", num)
+		dump("item1: %v, item2: %v\n", item1, item2)
+
+		p1 := item1.Priority()
+		p2 := item2.Priority()
+
+		expect := p1 * p2
+		ans = math.Max(ans, float64(expect))
+	}
+
+	fmt.Println(ans)
+}
+
+type hItem struct {
+	cnt int
+	K   int
+}
+
+func (h hItem) Priority() float64 {
+	return float64(h.cnt) / float64(h.K)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// 最小ヒープ（小さい値が優先）
+type HeapItem interface {
+	Priority() float64
+}
+
+type Heap[T HeapItem] []T
+
+func (h *Heap[T]) PushItem(item T) {
+	heap.Push(h, item)
+}
+
+func (h *Heap[T]) PopItem() T {
+	return heap.Pop(h).(T)
+}
+
+// to implement sort.Interface
+func (h Heap[T]) Len() int           { return len(h) }
+func (h Heap[T]) Less(i, j int) bool { return h[i].Priority() > h[j].Priority() }
+func (h Heap[T]) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+// DO NOT USE DIRECTLY.
+// to implement heap.Interface
+func (h *Heap[T]) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+
+	*h = append(*h, x.(T))
+}
+
+// DO NOT USE DIRECTLY.
+// to implement heap.Interface
+func (h *Heap[T]) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type IntHeap struct {
+	iarr        []int // iarrは昇順/降順になっているとは限らないため、インデックスアクセスしないこと。
+	IntHeapType IntHeapType
+}
+
+func NewIntHeap(t IntHeapType) *IntHeap {
+	return &IntHeap{
+		iarr:        make([]int, 0),
+		IntHeapType: t,
+	}
+}
+
+type IntHeapType int
+
+const (
+	MinIntHeap IntHeapType = iota // 小さい方が優先して取り出される
+	MaxIntHeap                    // 大きい方が優先して取り出される
+)
+
+// O(logN)
+func (h *IntHeap) PushI(i int) {
+	heap.Push(h, i)
+}
+
+// O(logN)
+func (h *IntHeap) PopI() int {
+	return heap.Pop(h).(int)
+}
+
+// to implement sort.Interface
+func (h *IntHeap) Len() int { return len(h.iarr) }
+func (h *IntHeap) Less(i, j int) bool {
+	if h.IntHeapType == MaxIntHeap {
+		return h.iarr[i] > h.iarr[j]
+	} else {
+		return h.iarr[i] < h.iarr[j]
+	}
+}
+func (h *IntHeap) Swap(i, j int) { h.iarr[i], h.iarr[j] = h.iarr[j], h.iarr[i] }
+
+// DO NOT USE DIRECTLY.
+// to implement heap.Interface
+func (h *IntHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	h.iarr = append(h.iarr, x.(int))
+}
+
+// DO NOT USE DIRECTLY.
+// to implement heap.Interface
+func (h *IntHeap) Pop() any {
+	oldiarr := h.iarr
+	n := len(oldiarr)
+	x := oldiarr[n-1]
+	h.iarr = oldiarr[0 : n-1]
+	return x
+}
 
 //////////////
 // Helpers //
