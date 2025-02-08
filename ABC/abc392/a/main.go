fd598761859ec133fee2bc318e2dbf8238d066e8
Tomoki108 <shinonome81@gmail.com>
2025-02-08 21:02:09 +0900

    a

--- ABC/abc392/a/main.go
+++ ABC/abc392/a/main.go
@@ -24,11 +24,70 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	As := readIntArr(r)
+
+	next := true
+
+	for next {
+		B_1 := As[0]
+		B_2 := As[1]
+		B_3 := As[2]
+
+		if B_1*B_2 == B_3 {
+			fmt.Println("Yes")
+			return
+		}
+
+		next = NextPermutation(As)
+	}
+
+	fmt.Println("No")
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// NOTE:
+// next := true; for next { some(sl); next = NextPermutation(sl); } で使う
+//
+// O(len(sl)*len(sl)!)
+// sl の要素を並び替えて、次の辞書順の順列にする
+func NextPermutation[T ~int | ~string](sl []T) bool {
+	n := len(sl)
+	i := n - 2
+
+	// Step1: 右から左に探索して、「スイッチポイント」を見つける:
+	// 　「スイッチポイント」とは、右から見て初めて「リストの値が減少する場所」です。
+	// 　例: [1, 2, 3, 6, 5, 4] の場合、3 がスイッチポイント。
+	for i >= 0 && sl[i] >= sl[i+1] {
+		i--
+	}
+
+	//　スイッチポイントが見つからない場合、最後の順列に到達しています。
+	if i < 0 {
+		return false
+	}
+
+	// Step2: スイッチポイントの右側の要素から、スイッチポイントより少しだけ大きい値を見つけ、交換します。
+	// 　例: 3 を右側で最小の大きい値 4 と交換。
+	j := n - 1
+	for sl[j] <= sl[i] {
+		j--
+	}
+	sl[i], sl[j] = sl[j], sl[i]
+
+	// Step3: スイッチポイントの右側を反転して、辞書順に次の順列を作ります。
+	// 　例: [1, 2, 4, 6, 5, 3] → [1, 2, 4, 3, 5, 6]。
+	reverse(sl[i+1:])
+	return true
+}
+
+func reverse[T ~int | ~string](sl []T) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
 
 //////////////
 // Helpers //
