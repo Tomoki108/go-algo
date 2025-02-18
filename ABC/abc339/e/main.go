0db5d09c6f1ca8c13492cbee4daa03c30dc5b304
Tomoki108 <shinonome81@gmail.com>
2025-02-18 22:34:32 +0900

    ac e

--- ABC/abc339/e/main.go
+++ ABC/abc339/e/main.go
@@ -24,46 +24,91 @@ func main() {
 	N, D := read2Ints(r)
 	As := readIntArr(r)
 
-	memos := make(map[string]int, N)
-	genKey := func(idx, lastNum int) string {
-		return fmt.Sprintf("%d-%d", idx, lastNum)
+	maxNum := 5*pow(10, 5) + 1
+	length := make([]int, maxNum) // length[i]: iという数字を最後に採用している場合の、最大の部分列の長さ
+
+	// dp[j]: jを採用する場合の最大の部分列の長さ.
+	// 何idx目まで処理したかは陽に持たず、0~N-1までのループで同じ配列を使い回す.
+	dp := NewSegTreeMax(length)
+	for i := 0; i < N; i++ {
+		left := max(As[i]-D, 0)         // As[i]を採用する場合に、そこに遷移できる最小の数
+		right := min(As[i]+D+1, maxNum) // As[i]を採用する場合に、そこに遷移できる最大の数+1
+
+		dp.Update(As[i], dp.Query(left, right)+1)
 	}
 
-	maxAns := 0
-
-	var dp func(idx, lastNum, currentLen int)
-	dp = func(idx, lastNum, currentLen int) {
-		if idx == N {
-			return
-		}
-		if currentLen+(N-idx) <= maxAns {
-			return
-		}
-
-		key := genKey(idx, lastNum)
-		if val, ok := memos[key]; ok && val >= currentLen {
-			return
-		}
-
-		if lastNum == -1 || abs(lastNum-As[idx]) <= D {
-			memos[genKey(idx, As[idx])] = currentLen + 1
-			maxAns = max(maxAns, currentLen+1)
-
-			dp(idx+1, As[idx], currentLen+1)
-		}
-
-		memos[genKey(idx, lastNum)] = currentLen
-		dp(idx+1, lastNum, currentLen)
-	}
-
-	dp(0, -1, 0)
-
-	fmt.Fprintln(w, maxAns)
+	ans := dp.Query(0, maxNum)
+	fmt.Fprintln(w, ans)
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// 区間最大値のセグメント木
+// セグメント木とは：https://qiita.com/Kept1994/items/d156a1ac1fe28553bf94
+type SegTreeMax struct {
+	n    int
+	size int
+	tree []int
+}
+
+func NewSegTreeMax(arr []int) *SegTreeMax {
+	n := len(arr)
+	size := 1
+	for size < n {
+		size *= 2
+	}
+	tree := make([]int, 2*size)
+	minVal := -1 << 63
+	for i := 0; i < 2*size; i++ {
+		tree[i] = minVal
+	}
+	for i, v := range arr {
+		tree[size+i] = v
+	}
+	for i := size - 1; i > 0; i-- {
+		tree[i] = max(tree[2*i], tree[2*i+1])
+	}
+	return &SegTreeMax{
+		n:    n,
+		size: size,
+		tree: tree,
+	}
+}
+
+// O(log N) N: 元々の配列の要素数
+// idx番目の値をvalueに更新
+func (st *SegTreeMax) Update(i, val int) {
+	i += st.size
+	st.tree[i] = val
+	for i > 1 {
+		i /= 2
+		st.tree[i] = max(st.tree[2*i], st.tree[2*i+1])
+	}
+}
+
+// O(log N) N: 元々の配列の要素数
+// [originL, originR) の範囲の最大値を取得
+func (st *SegTreeMax) Query(l, r int) int {
+	minVal := -1 << 63
+	res := minVal
+	l += st.size
+	r += st.size
+	for l < r {
+		if l%2 == 1 {
+			res = max(res, st.tree[l])
+			l++
+		}
+		if r%2 == 1 {
+			r--
+			res = max(res, st.tree[r])
+		}
+		l /= 2
+		r /= 2
+	}
+	return res
+}
 
 //////////////
 // Helpers  //
