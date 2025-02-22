f3d5f242d3ffddfa3d08a0c7ee11f7daba05748a
Tomoki108 <shinonome81@gmail.com>
2025-02-22 17:42:58 +0900

    c

--- ABC/abc302/c/main.go
+++ ABC/abc302/c/main.go
@@ -24,7 +24,7 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
-	N, M := read2Ints(r)
+	N, _ := read2Ints(r)
 
 	Ss := make([]map[string]struct{}, N)
 	for i := 0; i < N; i++ {
@@ -38,29 +38,87 @@ func main() {
 		Ss[i] = m
 	}
 
-	for i := 0; i < N-1; i++ {
-		ss1 := Ss[i]
-		ss2 := Ss[i+1]
+	indexes := make([]int, 0, N)
+	for i := 0; i < N; i++ {
+		indexes = append(indexes, i)
+	}
 
-		diff := 0
-		for k := range ss1 {
-			if _, ok := ss2[k]; !ok {
-				diff++
+	next := true
+	for next {
+
+		found := true
+		for i := 0; i < N-1; i++ {
+			ss1 := Ss[indexes[i]]
+			ss2 := Ss[indexes[i+1]]
+
+			diff := 0
+			for k := range ss1 {
+				if _, ok := ss2[k]; !ok {
+					diff++
+				}
+			}
+
+			if diff != 1 {
+				found = false
+				break
 			}
 		}
+		next = NextPermutation(indexes)
 
-		if diff != 1 {
-			fmt.Println("No")
+		if found {
+			fmt.Println("Yes")
 			return
 		}
 	}
 
-	fmt.Println("Yes")
+	fmt.Println("No")
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// NOTE:
+// next := true; for next { some(sl); next = NextPermutation(sl); } で使う
+// [重要] 最初の呼び出し時の引数には、昇順ソートしたスライスを渡すこと
+//
+// O(len(sl)*len(sl)!)
+// sl の要素を並び替えて、次の辞書順の順列にする
+func NextPermutation[T ~int | ~string](sl []T) bool {
+	n := len(sl)
+	i := n - 2
+
+	// Step1: 右から左に探索して、「スイッチポイント」を見つける:
+	// 　「スイッチポイント」とは、右から見て初めて「リストの値が減少する場所」です。
+	// 　例: [1, 2, 3, 6, 5, 4] の場合、3 がスイッチポイント。
+	for i >= 0 && sl[i] >= sl[i+1] {
+		i--
+	}
+
+	//　スイッチポイントが見つからない場合、最後の順列に到達しています。
+	if i < 0 {
+		return false
+	}
+
+	// Step2: スイッチポイントの右側の要素から、スイッチポイントより少しだけ大きい値を見つけ、交換します。
+	// 　例: 3 を右側で最小の大きい値 4 と交換。
+	j := n - 1
+	for sl[j] <= sl[i] {
+		j--
+	}
+	sl[i], sl[j] = sl[j], sl[i]
+
+	// Step3: スイッチポイントの右側を反転して、辞書順に次の順列を作ります。
+	// 　例: [1, 2, 4, 6, 5, 3] → [1, 2, 4, 3, 5, 6]。
+	reverse(sl[i+1:])
+	return true
+}
+
+func reverse[T ~int | ~string](sl []T) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
 
 //////////////
 // Helpers //
