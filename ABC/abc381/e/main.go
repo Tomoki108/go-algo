03364b22fcb026c8fd3891023cb252f17e7c9da7
Tomoki108 <shinonome81@gmail.com>
2025-02-26 14:57:36 +0900

    wa

--- ABC/abc381/e/main.go
+++ ABC/abc381/e/main.go
@@ -24,11 +24,118 @@ var w = bufio.NewWriter(os.Stdout)
 func main() {
 	defer w.Flush()
 
+	N, Q := read2Ints(r)
+	S := readStr(r)
+	Ss := strings.Split(S, "")
+
+	ones := make([]int, N)
+	twos := make([]int, N)
+	slashIndexes := make([]int, 0, N)
+	for i := 0; i < N; i++ {
+		if Ss[i] == "1" {
+			ones[i] = 1
+		} else if Ss[i] == "2" {
+			twos[i] = 1
+		} else {
+			slashIndexes = append(slashIndexes, i)
+		}
+	}
+
+	psumOne := PrefixSum(ones)
+	psumTwo := PrefixSum(twos)
+
+	for i := 0; i < Q; i++ {
+		L, R := read2Ints(r)
+		L--
+
+		oneCnt := psumOne[R] - psumOne[L]
+		// twoCnt := psumTwo[R] - psumTwo[L]
+
+		halfCnt := DescIntSearch(oneCnt, 1, func(cnt int) bool {
+			borderIdx := AscIntSearch(L, R, func(idx int) bool {
+				return psumOne[idx]-psumOne[L] >= cnt
+			})
+			if borderIdx == L-1 {
+				return false
+			}
+
+			centerIdx := sort.Sera
+
+			return psumTwo[R]-psumTwo[borderIdx] == cnt
+		})
+
+		if halfCnt == oneCnt+1 {
+			fmt.Fprintln(w, "0")
+		} else {
+			fmt.Fprintln(w, halfCnt*2+1)
+		}
+	}
 }
 
 //////////////
 // Libs    //
 /////////////
+
+// O(log(high-low))
+// low, low+1, ..., highの範囲で条件を満たす最小の値を二分探索する
+// low~highは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はlow-1を返す
+func AscIntSearch(low, high int, f func(num int) bool) int {
+	initialLow := low
+
+	for low < high {
+		// オーバーフローを防ぐための立式
+		// 中央値はlow側に寄る
+		mid := low + (high-low)/2
+		if f(mid) {
+			high = mid // 条件を満たす場合、よりlow側の範囲を探索
+		} else {
+			low = mid + 1 // 条件を満たさない場合、よりhigh側の範囲を探索
+		}
+	}
+
+	// 最後に low(=high) が条件を満たしているかを確認
+	if f(low) {
+		return low
+	}
+
+	return initialLow - 1 // 条件を満たす値が見つからない場合
+}
+
+// O(log(high-low))
+// high, high-1, ..., lowの範囲で条件を満たす最大の値を二分探索する
+// high~lowは条件に対して単調増加性を満たす必要がある
+// 条件を満たす値が見つからない場合はhigh+1を返す
+func DescIntSearch(high, low int, f func(num int) bool) int {
+	for low < high {
+		// オーバーフローを防ぐための式.
+		// 中央値はhigh側に寄る（+1しているため）
+		mid := low + (high-low+1)/2
+		if f(mid) {
+			low = mid // 条件を満たす場合、よりhigh側の範囲を探索
+		} else {
+			high = mid - 1 // 条件を満たさない場合、よりlow側の範囲を探索
+		}
+	}
+
+	// 最後に high(=low) が条件を満たしているかを確認
+	if f(high) {
+		return high
+	}
+
+	return high + 1 // 条件を満たす値が見つからない場合
+}
+
+// O(n)
+// 一次元累積和を返す（index0には0を入れる。）
+func PrefixSum(sl []int) []int {
+	n := len(sl)
+	res := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		res[i+1] = res[i] + sl[i]
+	}
+	return res
+}
 
 //////////////
 // Helpers //
