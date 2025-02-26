bfd3084794e12f61753f1696842ef4a4e14ff07a
Tomoki108 <shinonome81@gmail.com>
2025-02-26 15:58:41 +0900

    add lib

--- lib/bs/bs.go
+++ lib/bs/bs.go
@@ -1,6 +1,8 @@
 package bs
 
-// O(log(high-low))
+import "sort"
+
+// O(log (high-low))
 // low, low+1, ..., highの範囲で条件を満たす最小の値を二分探索する
 // low~highは条件に対して単調増加性を満たす必要がある
 // 条件を満たす値が見つからない場合はlow-1を返す
@@ -26,7 +28,7 @@ func AscIntSearch(low, high int, f func(num int) bool) int {
 	return initialLow - 1 // 条件を満たす値が見つからない場合
 }
 
-// O(log(high-low))
+// O(log (high-low))
 // high, high-1, ..., lowの範囲で条件を満たす最大の値を二分探索する
 // high~lowは条件に対して単調増加性を満たす必要がある
 // 条件を満たす値が見つからない場合はhigh+1を返す
@@ -48,4 +50,32 @@ func DescIntSearch(high, low int, f func(num int) bool) int {
 	}
 
 	return high + 1 // 条件を満たす値が見つからない場合
+}
+
+// O(log |sl|)
+// 単調増加性を満たすintスライスから、値がl以上、r以下の範囲を返す
+func RangeIntSearch(sl []int, l, r int) []int {
+	idx1 := sort.Search(len(sl), func(i int) bool { return sl[i] >= l })
+	if idx1 == len(sl) {
+		return nil
+	}
+	idx2 := sort.Search(len(sl), func(i int) bool { return sl[i] > r })
+	if idx2 == 0 {
+		return nil
+	}
+	return sl[idx1:idx2]
+}
+
+// O(log |sl|)
+// 単調増加性を満た任意型のスライスから、任意型が持つ単調増加性を満たす値がl以上、r以下の範囲を返す
+func RangeSearch[U any](sl []U, valuer func(item U) int, l, r int) []U {
+	idx1 := sort.Search(len(sl), func(i int) bool { return valuer(sl[i]) >= l })
+	if idx1 == len(sl) {
+		return nil
+	}
+	idx2 := sort.Search(len(sl), func(i int) bool { val := valuer(sl[i]); return val > r })
+	if idx2 == 0 {
+		return nil
+	}
+	return sl[idx1:idx2]
 }
--- lib/bs/bs_test.go
+++ lib/bs/bs_test.go
@@ -1,6 +1,9 @@
 package bs
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAscIntSearch(t *testing.T) {
 	sl1 := []int{1, 4, 5, 5, 6, 20}
@@ -87,5 +90,78 @@ func TestDescIntSearch(t *testing.T) {
 				t.Errorf("got: %v, want: %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestRangeIntSearch(t *testing.T) {
+	sl1 := []int{1, 1, 5, 5, 5, 6, 20, 21, 22, 22}
+	sl2 := []int{5}
+	sl3 := []int{9}
+
+	tests := []struct {
+		name string
+		sl   []int
+		l    int
+		r    int
+		want []int
+	}{
+		{
+			name: "example1",
+			sl:   sl1,
+			l:    5,
+			r:    21,
+			want: []int{5, 5, 5, 6, 20, 21},
+		},
+		{
+			name: "example2",
+			sl:   sl2,
+			l:    5,
+			r:    5,
+			want: []int{5},
+		},
+		{
+			name: "example3",
+			sl:   sl3,
+			l:    5,
+			r:    5,
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RangeIntSearch(tt.sl, tt.l, tt.r); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got: %v, want: %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRangeSearch(t *testing.T) {
+	type person struct {
+		Name string
+		Age  int
+	}
+
+	persons := []person{
+		{"Alice", 20},
+		{"Bob", 25},
+		{"Charlie", 30},
+		{"Dave", 35},
+		{"Eve", 40},
+	}
+
+	// valuer extracts the Age field.
+	ageValuer := func(p person) int { return p.Age }
+
+	// Expect to extract persons with Age between 26 and 39.
+	got := RangeSearch(persons, ageValuer, 26, 39)
+	want := []person{
+		{"Charlie", 30},
+		{"Dave", 35},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("RangeSearch(persons, 26, 39) = %v, want %v", got, want)
 	}
 }
