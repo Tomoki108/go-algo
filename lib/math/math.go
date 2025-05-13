4dad3bc4942bf586d1f61eaa9c9329f2cc321dff
Tomoki108 <shinonome81@gmail.com>
2025-05-13 20:19:30 +0900

    update lib

--- lib/math/math.go
+++ lib/math/math.go
@@ -19,7 +19,8 @@ func GetDigits(num int) int {
 // O(n) n: numの桁数
 // numの各桁の数字を返す
 func ToDigits(n int) []int {
-	s := strconv.FormatInt(int64(n), 10)
+	// s := strconv.FormatInt(int64(n), 10)
+	s := strconv.Itoa(n)
 	digits := make([]int, len(s))
 	for i := 0; i < len(s); i++ {
 		digits[i] = int(s[i] - '0') // （'x'に対応する数字 - '0'に対応する数字）のruneの数字 = x as int
